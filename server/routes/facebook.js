cdda8c6ac58f215623fbad287bb6067441ddab04
frlnd666 <74496050+frlnd666@users.noreply.github.com>
2025-02-07 10:33:47 +0700

    Update facebook.js

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -3,15 +3,22 @@ const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
 const path = require('path');
+const multer = require('multer');
 const router = express.Router();
 
 const accessToken = process.env.FACEBOOK_ACCESS_TOKEN;
 
-router.post('/upload', async (req, res) => {
-  const { videoPath, caption } = req.body;
+// Middleware untuk mengunggah file
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post('/upload', upload.single('file'), async (req, res) => {
+  const { caption } = req.body;
+  const videoBuffer = req.file.buffer;
+
   try {
     const formData = new FormData();
-    formData.append('file', fs.createReadStream(videoPath));
+    formData.append('file', videoBuffer, req.file.originalname);
     formData.append('caption', caption);
     formData.append('access_token', accessToken);
 
